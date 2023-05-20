18733c26121e8747ec4e6c1750b15c073f6dda5c
emondewan00 <dewanmohammademon@gmail.com>
2023-05-20 15:04:46 +0600

    product details section design done

--- src/pages/Details/ProductDetails.jsx
+++ src/pages/Details/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { BsFillBookmarkHeartFill } from "react-icons/bs";
 import img from "../../assets/spider-man.jpg";
 
 const ProductDetails = () => {
@@ -9,16 +10,42 @@ const ProductDetails = () => {
       <h1 className="text-4xl font-semibold text-center my-4 tracking-wide">
         Product Details
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 bg-base-100 shadow-xl">
+      <div className="grid grid-cols-1 md:grid-cols-2 bg-base-100 shadow-xl md:max-w-5xl mx-auto rounded">
         <figure>
           <img src={img} alt="Album" className="w-[500px]" />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Listen</button>
+        <div className=" w-4/5 mx-auto py-5 text-black space-y-3">
+          <h2 className="text-3xl font-semibold ">New album is released!</h2>
+          <p className="text-lg text-yellow-500 font-semibold">
+            $100.00 - <span className="line-through text-black">$50</span>
+          </p>
+          <p>ratings</p>
+          <input
+            type="number"
+            name=""
+            id=""
+            defaultValue="1"
+            className=" w-14  outline-none bg-gray-300 rounded-sm pl-2 "
+          />
+          <div className="card-actions items-center gap-x-5">
+            <button className="">
+              {" "}
+              <BsFillBookmarkHeartFill className="inline" /> Add to wishlist
+            </button>
+            <button className=" px-4 py-2 rounded-sm text-white hover:bg-[#ff4b0f] bg-[#ff630f] outline-none">Add to Cart</button>
           </div>
+          <h1 className="text-xl">product details</h1>
+          <ul className="list-disc grid grid-cols-1 md:grid-cols-2">
+            <li>saller</li>
+            <li>quntity</li>
+            <li>category</li>
+          </ul>
+          <h1 className="text-xl">product description</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est ad
+            inventore corporis at maxime ipsam similique aliquam, tempora omnis
+            hic in dolorem numquam rem modi architecto 
+          </p>
         </div>
       </div>
     </div>
