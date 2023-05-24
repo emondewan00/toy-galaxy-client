a30cd82bcad93526ba3d6cb7113ce60470fa5c3d
emondewan00 <dewanmohammademon@gmail.com>
2023-05-24 20:11:20 +0600

    add slider

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,19 +4,22 @@ import Category from "../Category/Category";
 import ImgGallery from "../imgGallery/ImgGallery";
 import CustomerReview from "../Review/CustomerReview";
 import WebTitle from "../../../webTitle/WebTitle";
+import Slider from "../slider/Slider";
 
 const Home = () => {
-  WebTitle()
+  WebTitle();
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center h-[80vh]  text-white font-serif  items-center my-10  " data-aos="fade-up">
+      <div
+        className="grid grid-cols-1 md:grid-cols-2 justify-items-center h-[80vh]  text-white font-serif  items-center my-10  "
+        data-aos="fade-up"
+      >
         <div className=" md:w-4/5 relative">
           <div className="w-80 h-80 bg-[#A427DF] absolute rounded-full blur-2xl opacity-20"></div>
           <div className="relative">
             <h1 className=" text-6xl px-4 md:px-0 ">
               It Help To Brain Growth For Your Childerns
             </h1>
-            
           </div>
         </div>
         <div>
@@ -28,6 +31,7 @@ const Home = () => {
         </div>
       </div>
       <Category />
+      <Slider />
       <ImgGallery />
       <CustomerReview />
     </div>
--- src/pages/Home/slider/Slider.jsx
+++ src/pages/Home/slider/Slider.jsx
@@ -1,28 +1,140 @@
 import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectCoverflow, Navigation, Pagination } from "swiper";
 
-const Slider = () => {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "./styles.css";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+function Slider() {
   return (
-    <div data-slick='{"slidesToShow": 4, "slidesToScroll": 4}' className="bg-white">
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
+    <div>
+      <h1 className="text-6xl text-center  text-white">UpComing Products</h1>
+
+      <Swiper
+        autoplay={{ delay: 1000 }}
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+          slideShadows: true,
+        }}
+        initialSlide="2"
+        pagination={{ el: ".swiper-pagination", clickable: true }}
+        navigation={{
+          prevEl: ".swiper-button-prev",
+          nextEl: ".swiper-button-next",
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation,Autoplay]}
+      >
+        <SwiperSlide>
+          <div className=" relative shadow-xl ">
+            <figure>
+              <img
+                src="/img-gallary/upComing.jpg"
+                className="w-[300px] h-[300px] rounded-lg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className=" h-full flex items-end absolute top-0 rounded-b-lg">
+              <div className="bg-white p-4 rounded-b-lg">
+                <h2 className="card-title">Action Figure</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" relative shadow-xl ">
+            <figure>
+              <img
+                src="/img-gallary/upComing2.jpg"
+                className="w-[300px] h-[300px] rounded-lg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className=" h-full flex items-end absolute top-0 rounded-b-lg">
+              <div className="bg-white p-4 rounded-b-lg">
+                <h2 className="card-title">Action Figure</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" relative shadow-xl ">
+            <figure>
+              <img
+                src="/img-gallary/upComing3.jpg"
+                className="w-[300px] h-[300px] rounded-lg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className=" h-full flex items-end absolute top-0 rounded-b-lg">
+              <div className="bg-white p-4 rounded-b-lg">
+                <h2 className="card-title">Action Figure</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" relative shadow-xl ">
+            <figure>
+              <img
+                src="/img-gallary/upComing4.jpg"
+                className="w-[300px] h-[300px] rounded-lg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className=" h-full flex items-end absolute top-0 rounded-b-lg">
+              <div className="bg-white p-4 rounded-b-lg">
+                <h2 className="card-title">Action Figure</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className=" relative shadow-xl ">
+            <figure>
+              <img
+                src="/img-gallary/gallary8.jpg"
+                className="w-[300px] h-[300px] rounded-lg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className=" h-full flex items-end absolute top-0 rounded-b-lg">
+              <div className="bg-white p-4 rounded-b-lg">
+                <h2 className="card-title">Action Figure</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <div className="flex  justify-around w-60 mx-auto items-center  mt-4">
+          <div className="swiper-button-prev p-2 cursor-pointer bg-white text-primary rounded-full">
+            <FaArrowLeft />
+          </div>
+          <div className="swiper-pagination cursor-pointer"> </div>
+          <div className="swiper-button-next cursor-pointer p-2 bg-white text-primary rounded-full">
+            <FaArrowRight />
+          </div>
+        </div>
+      </Swiper>
     </div>
   );
-};
+}
 
 export default Slider;
